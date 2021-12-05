3498cf44a9c6d38e1738cb8976a454cb7932f5dd
markmead <itsmarkmead@gmail.com>
2021-12-05 16:42:03 +0000

    Init
    Fix - CDN link
    
    update/improvements (#1)
    
    * Init
    
    * Update
    
    update/improvements (#1)
    
    * Init
    
    * Update
    
    update/improvements (#1)
    
    * Init
    
    * Update
    
    Init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export default function (Alpine) {
+  Alpine.directive('cookie', (el, { modifiers }, {}) => {
+    let useSessionStorage = modifiers[0] === 'session';
+
+    let closeButtonClicked = useSessionStorage
+      ? sessionStorage.getItem('_cookieBannerClose')
+      : localStorage.getItem('_cookieBannerClose');
+
+    closeButtonClicked = JSON.parse(closeButtonClicked) ?? false;
+
+    if (closeButtonClicked) {
+      el.setAttribute('hidden', true);
+    }
+
+    let closeButton = el.querySelector('button');
+
+    closeButton.addEventListener('click', function () {
+      el.setAttribute('hidden', true);
+
+      useSessionStorage
+        ? sessionStorage.setItem('_cookieBannerClose', true)
+        : localStorage.setItem('_cookieBannerClose', true);
+    });
+    // el.addEventListenter('click', (event) => {
+    //   console.log(event);
+    // });
+  });
+}
